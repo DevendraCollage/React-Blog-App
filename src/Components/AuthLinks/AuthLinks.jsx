a8b19352d64f60fea3a2733c632e07e0c2d53c58
Devendra Parmar <devendraparmarcollage@gmail.com>
2024-05-18 19:16:37 +0530

    Minor Bug Fixes and Implement the dark and light mode in the app and make the navbar responsive

--- src/Components/AuthLinks/AuthLinks.jsx
+++ src/Components/AuthLinks/AuthLinks.jsx
@@ -1,11 +1,13 @@
-import React from 'react'
-import styles from "./AuthLinks.module.css"
-import Link from 'next/link'
+import React, { useState } from "react";
+import styles from "./AuthLinks.module.css";
+import Link from "next/link";
 
 const AuthLinks = () => {
+  const [open, setOpen] = useState(false);
+
   //* This is temporary
-  const status = "Not Authenticated"
-  
+  const status = "Not Authenticated";
+
   return (
     <>
       {status === "Not Authenticated" ? (
@@ -16,8 +18,29 @@ const AuthLinks = () => {
           <span className={styles.Link}>Logout</span>
         </>
       )}
-    </>
-  )
-}
+      <div className={styles.burger} onClick={() => setOpen(!open)}>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+        <div className={styles.line}></div>
+      </div>
 
-export default AuthLinks
+      {open && (
+        <div className={styles.responsiveMenu}>
+          <Link href="/">Home</Link>
+          <Link href="/">About</Link>
+          <Link href="/">Contact</Link>
+          {status === "Not Authenticated" ? (
+            <Link href="/login">Login</Link>
+          ) : (
+            <>
+              <Link href="/write">Write</Link>
+              <span className={styles.Link}>Logout</span>
+            </>
+          )}
+        </div>
+      )}
+    </>
+  );
+};
+
+export default AuthLinks;
--- src/Components/ThemeToggle/ThemeToggle.jsx
+++ src/Components/ThemeToggle/ThemeToggle.jsx
@@ -1,15 +1,37 @@
-import React from 'react'
-import styles from "./ThemeToggle.module.css"
-import Image from 'next/image'
+"use client";
+
+import React, { useContext } from "react";
+import styles from "./ThemeToggle.module.css";
+import Image from "next/image";
+import { ThemeContext } from "@/Context/ThemeContext";
 
 const ThemeToggle = () => {
-  return (
-    <div className={styles.container}>
-      <Image src="/moon.png" alt='' height={14} width={14}/>
-      <div className={styles.ball}></div>
-      <Image src="/sun.png" alt='' height={14} width={14}/>
-    </div>
-  )
-}
+  const { theme, toggle } = useContext(ThemeContext);
 
-export default ThemeToggle
+  console.log(theme);
+
+  return (
+    <div
+      className={styles.container}
+      onClick={toggle}
+      style={
+        theme === "light"
+          ? { backgroundColor: "#0f172a" }
+          : { backgroundColor: "white" }
+      }
+    >
+      <Image src="/sun.png" alt="sun" width={16} height={16} />
+      <div
+        className={styles.ball}
+        style={
+          theme === "light"
+            ? { left: 1, background: "white" }
+            : { right: 1, background: "#0f172a" }
+        }
+      ></div>
+      <Image src="/moon.png" alt="moon" width={16} height={16} />
+    </div>
+  );
+};
+
+export default ThemeToggle;
